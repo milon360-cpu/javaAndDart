31f2ea7d3b0e77d9b120b641fc0f865a07215844
@heymilon <heymilon.com>
2021-03-03 05:09:32 +0600

    added

---
+++ Dart/practiceDart/dartSet.dart
@@ -0,0 +1,8 @@
+void main() {
+  var myList = new List();
+  var myListFixed = new List(3);
+  myListFixed[0] = 10;
+  myListFixed[1] = "Milon";
+  myListFixed[2] = "Tapu";
+  print(myListFixed);
+}
